0c3cced98676d6ff78c58401624beba1159df03f
AnnMurr <113177152+AnnMurr@users.noreply.github.com>
2025-05-23 17:26:10 +0300

    created goods app & product page

---
+++ goods/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def catalog(request):
+    context = {
+       'title': 'Vogue Vibe', 
+    }
+    return render(request, "goods/catalog.html", context)
+    
+# def product(request):
+#     render()
